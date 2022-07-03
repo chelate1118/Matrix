aa35ce4e91aa2863989d3a92af38dab7e4e892e4
chelate1118 <gs21077@gs.hs.kr>
2022-07-03 11:12:00 +0900

    키르히호프

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 #include "fraction.h"
 #include "fmatrix.h"
 
+int vertexNum, edgeNum;
+
+void solve() {
+    std::cin >> vertexNum >> edgeNum;
+
+    for (int i = 0; i < edgeNum; i++) {
+
+    }
+}
+
 int main() {
     std::freopen("./input/1.txt", "r", stdin);
-
-
 
     return 0;
 }
